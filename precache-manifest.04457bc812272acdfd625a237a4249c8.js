dc2033e4c1d8785de314b67f491a2945e52035f7
FomichtheDev <fomichg@gmail.com>
2020-10-27 16:07:02 +0200

    Updates

---
+++ precache-manifest.04457bc812272acdfd625a237a4249c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d402b9eec5238d353f7a626573b98b",
+    "url": "/wash-gel/index.html"
+  },
+  {
+    "revision": "bcbbe7542fd486e14cd5",
+    "url": "/wash-gel/static/css/main.ad632c5e.chunk.css"
+  },
+  {
+    "revision": "d95255b0b5c3d710f6d6",
+    "url": "/wash-gel/static/js/2.5b0a09cf.chunk.js"
+  },
+  {
+    "revision": "2f04377f6c78b08ab9b65546992c31dd",
+    "url": "/wash-gel/static/js/2.5b0a09cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcbbe7542fd486e14cd5",
+    "url": "/wash-gel/static/js/main.857402c7.chunk.js"
+  },
+  {
+    "revision": "31798df8a1d3f499a611",
+    "url": "/wash-gel/static/js/runtime-main.b4bc084c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wash-gel/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6778b691c1ad64d9484713e5610ef569",
+    "url": "/wash-gel/static/media/unnamed.6778b691.jpg"
+  }
+]);
